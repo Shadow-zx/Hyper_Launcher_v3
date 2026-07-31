8b5b1c5e5351d75eb180f5a9e4afd73d09f94d6c
hollowmc <hollowlauncher@gmail.com>
2026-07-31 15:24:05 +0530

    Update Hyper Launcher

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/LauncherMenuButton.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/LauncherMenuButton.java
@@ -33,15 +33,11 @@ public class LauncherMenuButton extends ExtendedButton {
     /** Set style stuff */
     private void setSettings(){
         Resources resources = getContext().getResources();
-
         int padding = resources.getDimensionPixelSize(R.dimen._22sdp);
         setCompoundDrawablePadding(padding);
         setPaddingRelative(padding, 0, 0, 0);
         setGravity(Gravity.CENTER_VERTICAL);
-
         setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._12ssp));
-
-        // Set drawable size
         int[] sizes = getExtendedViewData().getSizeCompounds();
         sizes[0] = resources.getDimensionPixelSize(R.dimen._30sdp);
         getExtendedViewData().setSizeCompounds(sizes);
